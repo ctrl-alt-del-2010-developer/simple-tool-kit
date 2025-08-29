75daf708d1a79d7d68fd4ee3f9b599abac5f85f1
ctrl-alt-del-2010-developer <amongus.iso07@gmail.com>
2025-08-29 22:32:36 +0300

    Update setup.sh

--- STK/setup.sh
+++ STK/setup.sh
@@ -5,7 +5,9 @@ YELLOW='\e[33m'
 RESET='\e[0m'
 
 clear
-xterm -e sudo apt install python2 python3 pip
+sudo apt install xterm 
+clear
+xterm -e sudo apt install python2 python3 pip cmatrix
 xterm -e pip3 install -r requirements.txt --break-system-packages
 clear
 echo -e "${GREEN}Installation complete. Enter the command 'bash run.sh' in the Command Prompt to start the program.${RESET}"
