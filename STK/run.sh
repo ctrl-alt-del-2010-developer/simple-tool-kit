aca5e47c8c1b7c66cb8123e9e21d8671f6a53a7e
ctrl-alt-del-2010-developer <amongus.iso07@gmail.com>
2025-08-29 22:35:16 +0300

    Update run.sh

--- STK/run.sh
+++ STK/run.sh
@@ -12,6 +12,7 @@ while true; do
     echo -e "${RED}[${YELLOW}02${RED}]${RESET} Tor IP"
     echo -e "${RED}[${YELLOW}03${RED}]${RESET} Scan Networks"
     echo -e "${RED}[${YELLOW}04${RED}]${RESET} DDoS Tool"
+    echo -e "${RED}[${YELLOW}05${RED}]${RESET} Cmatrix
     echo ""
     echo -e "${YELLOW}Tip:${RESET} Please Do Not Run This Program As Sudo."
     echo ""
@@ -39,6 +40,12 @@ while true; do
         4)  
             clear
             python3 ~/STK/Programs/DDoS-tool.py
+            clear
+            read -p "Press Enter Button For Continue..."
+            ;;
+        5)  
+            clear
+            xterm -e cmatrix
             clear
             read -p "Press Enter Button For Continue..."
             ;;
