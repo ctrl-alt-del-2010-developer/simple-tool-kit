9101e6bfc4474358ce3d8d148b9979a5ff035015
ctrl-alt-del-2010-developer <amongus.iso07@gmail.com>
2025-08-31 12:22:47 +0300

    Update run.sh

--- STK/run.sh
+++ STK/run.sh
@@ -9,7 +9,7 @@ while true; do
     clear
     cat ascii.txt  
     echo -e "${RED}[${YELLOW}01${RED}]${RESET} Red Hawk      ${RED}[${YELLOW}07${RED}]${RESET} Ping Test"
-    echo -e "${RED}[${YELLOW}02${RED}]${RESET} Tor IP        ${RED}[${YELLOW}08${RED}]${RESET} Firefox"
+    echo -e "${RED}[${YELLOW}02${RED}]${RESET} Tor IP        ${RED}[${YELLOW}08${RED}]${RESET} Tor Browser"
     echo -e "${RED}[${YELLOW}03${RED}]${RESET} Network Scan"
     echo -e "${RED}[${YELLOW}04${RED}]${RESET} DDoS Tool"
     echo -e "${RED}[${YELLOW}05${RED}]${RESET} Cmatrix"
@@ -61,7 +61,7 @@ while true; do
             read -p "Press Enter Button For Continue..."
             ;;
         8)  
-            firefox
+            torbrowser-launcher
             read -p "Press Enter Button For Continue..."
             ;;
         *)
