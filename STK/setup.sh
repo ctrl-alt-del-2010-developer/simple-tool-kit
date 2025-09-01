509927fe7b7db838c97aef0dce1f86290bebf6aa
ctrl-alt-del-2010-developer <amongus.iso07@gmail.com>
2025-09-01 11:42:34 +0300

    Update setup.sh

--- STK/setup.sh
+++ STK/setup.sh
@@ -7,7 +7,7 @@ RESET='\e[0m'
 clear
 sudo apt install xterm 
 clear
-xterm -e sudo apt install python2 python3 pip cmatrix aircrack-ng iwd torbrowser-launcher sqlmap
+xterm -e sudo apt install python2 python3 pip cmatrix aircrack-ng iwd torbrowser-launcher sqlmap wireshark htop
 xterm -e pip3 install -r requirements.txt --break-system-packages
 clear
 echo -e "${GREEN}Installation completed. Enter the command 'bash run.sh' in the Command Prompt to start the program.${RESET}"
